68a87326b8a46a69a406c58327c994e5384d4809
Mahendarreddy <mahendarredyykommidi82@gmail.com>
2024-05-30 12:59:59 +0530

    ''commit"

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -10,7 +10,7 @@ export  default function Hero() {
         fill="white"
       />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 mt-20">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 mt-10">
           Im Mahendar reddy
         </h1>
         <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
